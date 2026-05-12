5b4531e3f7d7f11ccb1a30acf13fc822b04877c5
Santiago Pinto <santiago.pinto.100@gmail.com>
2026-05-12 01:59:01 -0500

    fixed build issue

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -23,5 +23,6 @@ const content = `export const environment = {
 `;
 
 const outPath = path.join(__dirname, '../src/environments/environment.ts');
+fs.mkdirSync(path.dirname(outPath), { recursive: true });
 fs.writeFileSync(outPath, content);
 console.log('Generated src/environments/environment.ts');
