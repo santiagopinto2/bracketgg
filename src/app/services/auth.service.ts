2a8200786378db120361ff07c4b50c84bbc50c8f
Santiago Pinto <santiago.pinto.100@gmail.com>
2026-05-12 01:37:50 -0500

    fixed environment import

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, signal } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 interface TokenResponse {
   access_token: string;
